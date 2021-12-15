849fd0ca396fdcf5b77ad5a3e2b75b8deb72a85f
Joona <zockedidockhd@gmail.com>
2021-12-16 00:30:21 +0100

    draw pieces

--- public/index.js
+++ public/index.js
@@ -8,19 +8,23 @@ var square = {
     h: height / 8,
 };
 var pieces = [];
+var first_run = true;
 var drawBoard = function () {
     for (var y = 0; y < 8; ++y) {
         for (var x = 0; x < 8; ++x) {
             if ((y + x) % 2 == 0) {
                 drawRect(x * square.h, y * square.w, "#EEEEEE");
-                addPiece(x * square.h, y * square.w, "#EEEEEE");
+                if (first_run)
+                    addPiece(x, y, "#EEEEEE");
             }
             else {
                 drawRect(x * square.h, y * square.w, "black");
-                addPiece(x * square.h, y * square.w, "black");
+                if (first_run)
+                    addPiece(x, y, "black");
             }
         }
     }
+    first_run = false;
 };
 // function that takes x and y and draws a rectangle on the canvas
 var drawRect = function (x, y, color) {
@@ -30,50 +34,73 @@ var drawRect = function (x, y, color) {
 };
 var addPiece = function (x, y, color) {
     var piece = {
-        role: "ERROR",
+        role: "Empty",
         x: x,
         y: y,
-        color: color,
+        color: color == "black" ? "black" : "white",
         value: -1,
+        img: null,
     };
-    if (y === square.h || y === square.h * 6) {
+    if (y === 1 || y === 6) {
         piece.role = "pawn";
         piece.value = 1;
+        piece.img = new Image();
     }
     else if ((x === 0 && y === 0) ||
-        (x === square.w * 7 && y === 0) ||
-        (x === 0 && y === square.h * 7) ||
-        (x === square.w * 7 && y === square.h * 7)) {
+        (x === 7 && y === 0) ||
+        (x === 0 && y === 7) ||
+        (x === 7 && y === 7)) {
         piece.role = "rook";
         piece.value = 5;
+        piece.img = new Image();
     }
-    else if ((x === square.w * 1 && y === 0) ||
-        (x === square.w * 6 && y === 0) ||
-        (x === square.w * 1 && y === square.h * 7) ||
-        (x === square.w * 6 && y === square.h * 7)) {
+    else if ((x === 1 && y === 0) ||
+        (x === 6 && y === 0) ||
+        (x === 1 && y === 7) ||
+        (x === 6 && y === 7)) {
         piece.role = "knight";
         piece.value = 3;
+        piece.img = new Image();
     }
-    else if ((x === square.w * 2 && y === 0) ||
-        (x === square.w * 5 && y === 0) ||
-        (x === square.w * 2 && y === square.h * 7) ||
-        (x === square.w * 5 && y === square.h * 7)) {
+    else if ((x === 2 && y === 0) ||
+        (x === 5 && y === 0) ||
+        (x === 2 && y === 7) ||
+        (x === 5 && y === 7)) {
         piece.role = "bishop";
         piece.value = 3;
+        piece.img = new Image();
     }
-    else if ((x === square.w * 3 && y === 0) ||
-        (x === square.w * 3 && y === square.h * 7)) {
+    else if ((x === 3 && y === 0) || (x === 3 && y === 7)) {
         piece.role = "queen";
         piece.value = 9;
+        piece.img = new Image();
     }
-    else if ((x === square.w * 4 && y === 0) ||
-        (x === square.w * 4 && y === square.h * 7)) {
+    else if ((x === 4 && y === 0) || (x === 4 && y === 7)) {
         piece.role = "king";
         piece.value = 100;
-    }
-    else {
-        return;
+        piece.img = new Image();
     }
     pieces.push(piece);
 };
+var drawPieces = function () {
+    var _loop_1 = function (piece) {
+        if (piece.role !== "Empty") {
+            piece.img.src = "./".concat(piece.color, "/").concat(piece.role, ".png");
+            piece.img.onload = function () {
+                return ctx.drawImage(piece.img, piece.x * square.w, piece.y * square.h, square.w, square.h);
+            };
+        }
+    };
+    for (var _i = 0, pieces_1 = pieces; _i < pieces_1.length; _i++) {
+        var piece = pieces_1[_i];
+        _loop_1(piece);
+    }
+};
 drawBoard();
+drawPieces();
+document.getElementById("mp").addEventListener("click", function () {
+    pieces[10].y += 2;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBoard();
+    drawPieces();
+});
--- src/index.ts
+++ src/index.ts
@@ -11,10 +11,11 @@ interface Piece {
     y: number
     color: string
     value: number
+    img: HTMLImageElement
 }
 
 const canvas = document.getElementById("canvas") as HTMLCanvasElement
-const ctx = canvas.getContext("2d")
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D
 let width = canvas.width
 let height = canvas.height
 let square: Square = {
@@ -22,76 +23,102 @@ let square: Square = {
     h: height / 8,
 }
 var pieces: Piece[] = []
+var first_run = true
 
-const drawBoard = () => {
+const drawBoard = (): void => {
     for (let y = 0; y < 8; ++y) {
         for (let x = 0; x < 8; ++x) {
             if ((y + x) % 2 == 0) {
                 drawRect(x * square.h, y * square.w, "#EEEEEE")
-                addPiece(x * square.h, y * square.w, "#EEEEEE")
+                if (first_run) addPiece(x, y, "#EEEEEE")
             } else {
                 drawRect(x * square.h, y * square.w, "black")
-                addPiece(x * square.h, y * square.w, "black")
+                if (first_run) addPiece(x, y, "black")
             }
         }
     }
+    first_run = false
 }
 // function that takes x and y and draws a rectangle on the canvas
-const drawRect = (x: number, y: number, color: string = "white") => {
+const drawRect = (x: number, y: number, color: string = "white"): void => {
     ctx.fillStyle = color
     ctx.fillRect(x, y, square.w, square.h)
 }
 
-const addPiece = (x: number, y: number, color: string) => {
+const addPiece = (x: number, y: number, color: string): void => {
     let piece: Piece = {
-        role: "ERROR",
+        role: "Empty",
         x: x,
         y: y,
-        color: color,
+        color: color == "black" ? "black" : "white",
         value: -1,
+        img: null,
     }
-    if (y === square.h || y === square.h * 6) {
+    if (y === 1 || y === 6) {
         piece.role = "pawn"
         piece.value = 1
+        piece.img = new Image()
     } else if (
         (x === 0 && y === 0) ||
-        (x === square.w * 7 && y === 0) ||
-        (x === 0 && y === square.h * 7) ||
-        (x === square.w * 7 && y === square.h * 7)
+        (x === 7 && y === 0) ||
+        (x === 0 && y === 7) ||
+        (x === 7 && y === 7)
     ) {
         piece.role = "rook"
         piece.value = 5
+        piece.img = new Image()
     } else if (
-        (x === square.w * 1 && y === 0) ||
-        (x === square.w * 6 && y === 0) ||
-        (x === square.w * 1 && y === square.h * 7) ||
-        (x === square.w * 6 && y === square.h * 7)
+        (x === 1 && y === 0) ||
+        (x === 6 && y === 0) ||
+        (x === 1 && y === 7) ||
+        (x === 6 && y === 7)
     ) {
         piece.role = "knight"
         piece.value = 3
+        piece.img = new Image()
     } else if (
-        (x === square.w * 2 && y === 0) ||
-        (x === square.w * 5 && y === 0) ||
-        (x === square.w * 2 && y === square.h * 7) ||
-        (x === square.w * 5 && y === square.h * 7)
+        (x === 2 && y === 0) ||
+        (x === 5 && y === 0) ||
+        (x === 2 && y === 7) ||
+        (x === 5 && y === 7)
     ) {
         piece.role = "bishop"
         piece.value = 3
-    } else if (
-        (x === square.w * 3 && y === 0) ||
-        (x === square.w * 3 && y === square.h * 7)
-    ) {
+        piece.img = new Image()
+    } else if ((x === 3 && y === 0) || (x === 3 && y === 7)) {
         piece.role = "queen"
         piece.value = 9
-    } else if (
-        (x === square.w * 4 && y === 0) ||
-        (x === square.w * 4 && y === square.h * 7)
-    ) {
+        piece.img = new Image()
+    } else if ((x === 4 && y === 0) || (x === 4 && y === 7)) {
         piece.role = "king"
         piece.value = 100
-    } else {
-        return
+        piece.img = new Image()
     }
     pieces.push(piece)
 }
+
+const drawPieces = (): void => {
+    for (let piece of pieces) {
+        if (piece.role !== "Empty") {
+            piece.img.src = `./${piece.color}/${piece.role}.png`
+            piece.img.onload = () =>
+                ctx.drawImage(
+                    piece.img,
+                    piece.x * square.w,
+                    piece.y * square.h,
+                    square.w,
+                    square.h
+                )
+        }
+    }
+}
+
 drawBoard()
+drawPieces()
+
+document.getElementById("mp").addEventListener("click", () => {
+    pieces[10].y += 2
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    drawBoard()
+    drawPieces()
+})
